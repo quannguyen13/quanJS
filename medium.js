c0e4e999853c02dfb332f31e924f623e44bb59df
quannguyen <waynenguyenus@gmail.com>
2022-04-24 10:17:53 -0700

    10

--- medium.js
+++ medium.js
@@ -14,6 +14,9 @@ const oddishOrEvenish = function(number){
 // oddishOrEvenish(373)
 // oddishOrEvenish(43)
 
+
+
+
 // !__________________________________________
 // Create a function that takes an array of numbers and return "Boom!" if the digit 7 appears in the array. Otherwise, return "there is no 7 in the array".
 //?https://edabit.com/challenge/6R6gReGTGwzpwuffD
