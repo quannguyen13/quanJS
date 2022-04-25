835e97d475dcd27914b030e06cb69c7753634ffd
quannguyen <waynenguyenus@gmail.com>
2022-04-25 15:34:32 -0700

    1

--- medium.js
+++ medium.js
@@ -19,26 +19,17 @@ const oddishOrEvenish = function(number){
 // Create a function that takes an array of numbers and return "Boom!" if the digit 7 appears in the array. Otherwise, return "there is no 7 in the array".
 //?https://edabit.com/challenge/6R6gReGTGwzpwuffD
 
-// function sevenBoom(array) {
-//     // var transform = array.toString().split(",")
-//     var transform = array.toString()
-//     var check = transform.includes("7")
-//     console.log(transform);
-//     console.log(check);
-// }
-// sevenBoom([2, 55, 60, 97, 86])
 function sevenBoom(array) {
-    // var transform = array.toString().split(",")
     var transform = array.toString()
     var check = transform.includes("7")
-    console.log(transform);
-    console.log(check);
+    // console.log(transform);
+    // console.log(check);
     if (check) { 
-        for (var i = 0; i < transform.length; i++) {
-            console.log(`YES!!! number: ${transform[i]} contain 7`);
-        }
+        console.log(`YES!!! It is contain 7`);
     } else {
         console.log("NOPE!!! NO number 7 existing");
     }
 }
+sevenBoom([1, 2, 3, 4, 5, 6, 7])
+sevenBoom([8, 6, 33, 100]) 
 sevenBoom([2, 55, 60, 97, 86])
