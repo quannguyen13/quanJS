70140865c22fa399e30e5da142a17a9d04a86772
quannguyen <waynenguyenus@gmail.com>
2022-04-28 21:24:41 -0700

    12

--- easyObject.js
+++ easyObject.js
@@ -14,6 +14,13 @@ function getVoteCount(votes) {
 
 
 // Luke Skywalker has family and friends. Help him remind them who is who. Given a string with a name, return the relation of that person to Luke.
+
+// Examples
+// relationToLuke("Darth Vader") ➞ "Luke, I am your father."
+
+// relationToLuke("Leia") ➞ "Luke, I am your sister."
+
+// relationToLuke("Han") ➞ "Luke, I am your brother in law."
 const relateToLuke = [
     {
         person: "Darth Vader",
