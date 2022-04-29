3165948eab09fb22d65bbbbbacebbc1f4e17ed3a
quannguyen <waynenguyenus@gmail.com>
2022-04-28 17:40:58 -0700

    1

---
+++ object.js
@@ -0,0 +1,11 @@
+const car = {
+    make: 'Honda',
+    model: 'Civic',
+    year: 1997,
+    'tire-type': 'Goodyear',
+  
+  };
+
+console.log(car.make);
+console.log( car['make'] )
+console.log( car.owner )
