49f1742e6b7669ac44d8ee5c75ac3a2ef599c815
quannguyen <waynenguyenus@gmail.com>
2022-04-28 18:14:17 -0700

    2

--- object.js
+++ object.js
@@ -6,6 +6,28 @@ const car = {
   
   };
 
-console.log(car.make);
-console.log( car['make'] )
-console.log( car.owner )
+// console.log(car.make);
+// console.log( car['make'] )
+// console.log( car["tire-type"] )
+
+const vacation = {
+  where: "Viet Nam",
+  tripStart: "May 15",
+  tripEnd: "June 11",
+  purchasedTickets: false,
+  "air-plane": false
+
+}
+//  dot notation
+console.log(vacation.where);
+// bracket notation
+console.log(vacation["where"]);
+
+
+// 
+function getProp(propName){
+  console.log(vacation[propName]);
+ 
+}
+getProp("tripStart")
+// getProp(where)
