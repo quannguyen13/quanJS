f088b98a3c250817867fd27da999cf02f6747b9f
quannguyen <waynenguyenus@gmail.com>
2022-04-30 18:11:30 -0700

    4

--- object.js
+++ object.js
@@ -1,5 +1,8 @@
-const sayHi = () => {
-  console.log(`say Hi from sayHi()`, this);
+// const sayHi = () => {
+//   console.log(`say Hi from sayHi()`, this);
+// }
+function sayHi(){
+  return `say Hi from sayHi() \n ${this.make}`
 }
 const car = {
     make: 'Honda',
@@ -27,6 +30,8 @@ const car = {
     }
   };
 
+var result = sayHi.bind(car)
+// console.log(result());
 // console.log(car.make);
 // console.log( car['make'] )
 // console.log( car["tire-type"] )
@@ -103,25 +108,25 @@ for (i in car) {
 // console.log(Object.entries(vacation.air_plane));
 
 
-const person = {
-  firstName: "",
-  lastName: ""
-}
+// const person = {
+//   firstName: "",
+//   lastName: ""
+// }
 
 
-person.firstName = "Quan"
-person.lastName = "Nguyen"
-person["pets"] = ""
+// person.firstName = "Quan"
+// person.lastName = "Nguyen"
+// person["pets"] = ""
 
-if (person["pets"]) {
+// if (person["pets"]) {
   
   // console.log(`my name is ${person.firstName} ${person.lastName} and i used to have a dog name ${person.pets}`);
-} else {
+// } else {
   // console.log(`my name is ${person.firstName} ${person.lastName} and i have NO pet`);
   
-}
+// }
 
-delete person["pets"]
+// delete person["pets"]
 // console.log(person);
 
 
@@ -164,11 +169,29 @@ const user = {
     this.isSignedIn = false;
   },
 };
-console.log("Before sign in", user.isSignedIn);
-user.signIn('tacobeelll', 'christensen' )
-// user.signIn('password1234', 'christensen' )
+// console.log("Before sign in", user.isSignedIn);
+// user.signIn('tacobeelll', 'christensen' )
+// // user.signIn('password1234', 'christensen' )
 
-console.log("POST sign in", user.isSignedIn);
+// console.log("POST sign in", user.isSignedIn);
 
-user.signOut();
-console.log("POST sign out", user.isSignedIn);
+// user.signOut();
+// console.log("POST sign out", user.isSignedIn);
+function sayHello1() {
+  console.log(`Hi! My name is ${this.name}`);
+}
+
+const person1 = { name: 'Manatee the Railyard Toreador' };
+const cat1 = { name: 'Hobbles McGillicudy' };
+sayHello1.call(person1);
+sayHello1.call(cat1);
+// call also lets us pass in the arguments to the function:
+
+function sayHello2(favColor) {
+  console.log(`Hi! My name is ${this.name} and I like ${favColor}`);
+}
+
+const person2 = { name: 'Manatee the Railyard Toreador' };
+const cat2 = { name: 'Hobbles McGillicudy' };
+sayHello2.call(person2, 'blue');
+sayHello2.call(cat2, 'peachpuff');
