47b5177a59b7190f7057b7e183002c8725a847fa
quannguyen <waynenguyenus@gmail.com>
2022-05-01 10:21:06 -0700

    2

--- easyJS.js
+++ easyJS.js
@@ -341,4 +341,26 @@ function arrayOfMultiples(number, length){
 
 // !__________________________________________
 
+// Write a function that returns true if there exists at least one number that is larger than or equal to n.
 
+// Examples
+// existsHigher([5, 3, 15, 22, 4], 10) ➞ true
+
+// existsHigher([1, 2, 3, 4, 5], 8) ➞ false
+
+// existsHigher([4, 3, 3, 3, 2, 2, 2], 4) ➞ true
+
+// existsHigher([], 5) ➞ false
+function existsHigher() {
+    var newArray = []
+    for (let i = 0; i < arguments.length; i++) {
+        newArray.push(arguments[i]);
+        
+    }
+    var check = newArray[0].some(e => e > newArray[1])
+    console.log(check);
+}
+
+// existsHigher([5, 3, 15, 22, 4], 10)
+// existsHigher([4, 3, 3, 3, 2, 2, 2], 4)
+// existsHigher([1, 2, 3, 4, 5], 8)
