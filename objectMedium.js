ce55e691b70c3675c13862df6c9f69d5598a9816
quannguyen <waynenguyenus@gmail.com>
2022-05-05 22:58:02 -0700

    13

--- objectMedium.js
+++ objectMedium.js
@@ -16,12 +16,12 @@ function getTotalPrice(array) {
     var totalBill = array.map(e => e.quantity * e.price).reduce((a, b) => a + b, 0)
     console.log(totalBill.toFixed(2));
 }
-getTotalPrice([
-    { product: "Milk", quantity: 1, price: 1.50 },
-    { product: "Eggs", quantity: 12, price: 0.10 },
-    { product: "Bread", quantity: 22, price: 1.60 },
-    { product: "Cheese", quantity: 1, price: 4.50 }
-  ]) 
+// getTotalPrice([
+//     { product: "Milk", quantity: 1, price: 1.50 },
+//     { product: "Eggs", quantity: 12, price: 0.10 },
+//     { product: "Bread", quantity: 22, price: 1.60 },
+//     { product: "Cheese", quantity: 1, price: 4.50 }
+//   ]) 
 // getTotalPrice([
 //     { product: "Milk", quantity: 3, price: 1.50 }
 //   ])
