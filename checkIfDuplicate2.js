8caf64e8f2e86d92ce8b234056ace837ef517116
quannguyen <waynenguyenus@gmail.com>
2022-05-07 18:08:44 -0700

    2

--- checkIfDuplicate2.js
+++ checkIfDuplicate2.js
@@ -1,9 +1,23 @@
 // var arr = ['AAA', 'XYZ', 'AAA', 'ABC', 'XXX', 'XYZ', 'PQR']
 var arr = ["Red", "Green", "Yellow", "Blue", "Blue"]
+function check(arr){
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== arr[i + 1]) {
+            // console.log(`do first`, arr[i], arr[i + 1]);
+            i++
+            return true
+        } else {
+
+            return false
+        }
+        
+    }
+    
+}
 var checkIfDuplicate = arr.some((ele, i) => arr.indexOf(ele) !== i)
 // console.log(checkIfDuplicate)
-var check = arr.map((ele => arr.indexOf(ele)))
-console.log(check);
-
+// var check = arr.map((ele => arr.indexOf(ele)))
+// console.log(i);
+console.log(check(arr));
 
 
--- colorsChallenge.js
+++ colorsChallenge.js
@@ -9,23 +9,43 @@
 // 10 + 4 = 14
 
 // https://edabit.com/challenge/Np7R8F24PaqBShZc5
+
 function colorPatternTimes(array) {
     var switchPencilsTiming = (array.length - 1)
     var numberOfColors = 0
     for (let i = 0; i < array.length; i++) {
-        if (array[i] == array[i + 1]) {
-            numberOfColors = 1
-        }
-        else if (array[i] !== array[i + 1]){
+        // if (array[i] == array[i + 1]) {
+        //     numberOfColors = 1
+        // }
+        // else 
+        if (array[i] !== array[i + 1] ){
             numberOfColors += 1;
         }
     }
-    console.log(numberOfColors);
+    // console.log(numberOfColors);
     var colors = numberOfColors * 2
     var time = switchPencilsTiming + colors
-    // console.log(time);
+    console.log(time);
 }
-// colorPatternTimes(["Blue", "Blue", "Blue", "Red", "Red", "Red"])
+
+
+// !!__________________________________________________________________________________
+// !! ANDRE SOLUTION
+// function colorPatternTimes(array) {  
+//     let swithchColors = 0
+//     for (let i = 0; i < array.length; i++) {
+
+//         if (array[i] !== array[i + 1] && array[i + 1] !== undefined){
+//             swithchColors += 1;
+//             console.log("first call", array[i],"second call", array[i + 1]);
+//         }
+//     }
+
+//     let colors = array.length * 2
+//     let totalTime = swithchColors + colors
+//     console.log(totalTime);
+// }
+colorPatternTimes(["Blue", "Blue", "Blue", "Red", "Red", "Red"])
 // colorPatternTimes(["Red", "Yellow", "Green", "Blue", "Red"])
 // colorPatternTimes(["Red", "Yellow", "Green", "Blue"])
 // colorPatternTimes(["Blue"])
