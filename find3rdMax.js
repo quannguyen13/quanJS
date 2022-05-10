29c49598247449c3d39ab5e6a2560763f806fd95
quannguyen <waynenguyenus@gmail.com>
2022-05-09 21:46:46 -0700

    3

--- find3rdMax.js
+++ find3rdMax.js
@@ -4,7 +4,7 @@
 // output: 1
 // explanation: first max = 3, second max = 2. third max = 1
 
-var numbers = [2, 1, 3]
+var numbers = [2, 1, 3, 0, 12, 22, 32]
 function thirdMax(array) {
     if (array.length >= 3) {
         var third = array.sort((a, b) => b - a).slice(0, 3)[2]
