c601d16eb788c0566713103c8fc359f01198cfe3
quannguyen <waynenguyenus@gmail.com>
2022-05-10 21:08:13 -0700

    5

--- hard.js
+++ hard.js
@@ -11,12 +11,15 @@ function getLength(array) {
 // !________________________________________
 // ? write a function just to return element include nunmber
 // https://edabit.com/challenge/eCPim4XcssdZdvs32
+// source: https://stackoverflow.com/questions/3180354/regex-check-if-string-contains-at-least-one-digit
 function numInStr(array) {
     var check = array.filter(e => e.match(/\d/) ? e : null)
     console.log(check);
 }
 
-numInStr(["1a", "a", "2b", "b"])
-numInStr(["abc", "abc10"])
-numInStr(["abc", "ab10c", "a10bc", "bcd"])
-numInStr(["this is a test", "test1"])
+// numInStr(["1a", "a", "2b", "b"])
+// numInStr(["abc", "abc10"])
+// numInStr(["abc", "ab10c", "a10bc", "bcd"])
+// numInStr(["this is a test", "test1"])
+
+// !________________________________________
