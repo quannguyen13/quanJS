6c1e0e7d6c8e77c121a2a631365200ac35b435fe
quannguyen <waynenguyenus@gmail.com>
2022-05-10 22:15:29 -0700

    12

--- hard.js
+++ hard.js
@@ -29,51 +29,63 @@ function numInStr(array) {
 // const uniqueAges = ages.filter((x, i, a) => a.indexOf(x) == i)
 // console.log(uniqueAges)
 // ? CASE 1: use for filter() to filter positive and negative number
-function isPositiveDominant(array) {
-    var positiveArray = array.filter(e => e > 0 ? e : null)
-    // console.log(positiveArray);
-    var uniquePositiveArray = positiveArray.filter((x, i, a) => a.indexOf(x) == i)
-    // console.log(uniquePositiveArray);
-    var negativeArray = array.filter(e => e < 0 ? e : null)
-    var uniqueNegativeArray = negativeArray.filter((x, i, a) => a.indexOf(x) == i)
-    // console.log(uniqueNegativeArray);
-    if (uniquePositiveArray.length > uniqueNegativeArray.length) {
-        console.log(true);
-    } else {
-        console.log(false);
-    }
-}
+// function isPositiveDominant(array) {
+//     var positiveArray = array.filter(e => e > 0 ? e : null)
+//     // console.log(positiveArray);
+//     var uniquePositiveArray = positiveArray.filter((x, i, a) => a.indexOf(x) == i)
+//     // console.log(uniquePositiveArray);
+//     var negativeArray = array.filter(e => e < 0 ? e : null)
+//     var uniqueNegativeArray = negativeArray.filter((x, i, a) => a.indexOf(x) == i)
+//     // console.log(uniqueNegativeArray);
+//     if (uniquePositiveArray.length > uniqueNegativeArray.length) {
+//         console.log(true);
+//     } else {
+//         console.log(false);
+//     }
+// }
 
 
 
 // ? CASE 2: use for loop to filter positive and negative number
 function isPositiveDominant(array) {
-
-    // var positiveArray = []
-    // for (let i = 0; i < array.length; i++) {
-    //     if (array[i] >= 0) {
-    //         if (array[i] !== array[i + 1]) {
-    //             var positive = array[i]
-    //             positiveArray.push(positive)
-    //             // console.log(positive);
-    //         }
-    //         else if (array[i] === array[i + 1]){
-    //             var unique = array[i]
-    //             positiveArray.push(unique)
+    var positiveArray = []
+    var negativeArray = []
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] >= 0) {
+            if (array[i] !== array[i + 1]) {
+                var positive = array[i]
+                positiveArray.push(positive)
+                // console.log(positive);
+            }
+            else if (array[i] === array[i + 1]){
+                var unique = array[i]
+                positiveArray.push(unique)
                 
-    //             // console.log(unique);
-    //         }
+                // console.log(unique);
+            }
             
-    //     }
+        }
         
-    // }
+    }
+    for (let j = 0; j < array.length; j++) {
+        if (array[j] < 0) {
+            var negative = array[j]
+            negativeArray.push(negative)
+        }
+        
+    }
     // console.log(positiveArray);
+    // console.log(negativeArray);
+    var uniqueNegative = negativeArray.filter((e, i, a) => a.indexOf(e) == i)
+    var uniquePositive = positiveArray.filter((e, i, a) => a.indexOf(e) == i)
+    console.log(uniqueNegative);
+    console.log(uniquePositive);
 }
 
 
 
 // isPositiveDominant([1, 1, 1, 1, -3, -3])
 isPositiveDominant([1, 1, 1, 1, -3, -4])
-isPositiveDominant([5, 99, 832, -3, -4])
-isPositiveDominant([5, 0])
-isPositiveDominant([0, -4, -1])
+// isPositiveDominant([5, 99, 832, -3, -4])
+// isPositiveDominant([5, 0])
+// isPositiveDominant([0, -4, -1])
