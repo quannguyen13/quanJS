aed7447442c77f5b34fe4005c92fd22ab922bf82
quannguyen <waynenguyenus@gmail.com>
2022-05-10 22:11:19 -0700

    11

--- hard.js
+++ hard.js
@@ -28,8 +28,27 @@ function numInStr(array) {
 // const ages = [26, 27, 26, 26, 28, 28, 29, 29, 30]
 // const uniqueAges = ages.filter((x, i, a) => a.indexOf(x) == i)
 // console.log(uniqueAges)
+// ? CASE 1: use for filter() to filter positive and negative number
 function isPositiveDominant(array) {
-    // ? CASE 1: use for loop to filter positive and negative number
+    var positiveArray = array.filter(e => e > 0 ? e : null)
+    // console.log(positiveArray);
+    var uniquePositiveArray = positiveArray.filter((x, i, a) => a.indexOf(x) == i)
+    // console.log(uniquePositiveArray);
+    var negativeArray = array.filter(e => e < 0 ? e : null)
+    var uniqueNegativeArray = negativeArray.filter((x, i, a) => a.indexOf(x) == i)
+    // console.log(uniqueNegativeArray);
+    if (uniquePositiveArray.length > uniqueNegativeArray.length) {
+        console.log(true);
+    } else {
+        console.log(false);
+    }
+}
+
+
+
+// ? CASE 2: use for loop to filter positive and negative number
+function isPositiveDominant(array) {
+
     // var positiveArray = []
     // for (let i = 0; i < array.length; i++) {
     //     if (array[i] >= 0) {
@@ -49,24 +68,10 @@ function isPositiveDominant(array) {
         
     // }
     // console.log(positiveArray);
-
-
-    // ? CASE 2: use for filter() to filter positive and negative number
-
-
-    var positiveArray = array.filter(e => e > 0 ? e : null)
-    // console.log(positiveArray);
-    var uniquePositiveArray = positiveArray.filter((x, i, a) => a.indexOf(x) == i)
-    // console.log(uniquePositiveArray);
-    var negativeArray = array.filter(e => e < 0 ? e : null)
-    var uniqueNegativeArray = negativeArray.filter((x, i, a) => a.indexOf(x) == i)
-    // console.log(uniqueNegativeArray);
-    if (uniquePositiveArray.length > uniqueNegativeArray.length) {
-        console.log(true);
-    } else {
-        console.log(false);
-    }
 }
+
+
+
 // isPositiveDominant([1, 1, 1, 1, -3, -3])
 isPositiveDominant([1, 1, 1, 1, -3, -4])
 isPositiveDominant([5, 99, 832, -3, -4])
