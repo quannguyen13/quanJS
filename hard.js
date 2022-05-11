20788398065fd5d6dda03116da93dd582cc53d7d
quannguyen <waynenguyenus@gmail.com>
2022-05-10 21:06:57 -0700

    4

--- hard.js
+++ hard.js
@@ -7,3 +7,16 @@ function getLength(array) {
 // getLength([1, [2, [3, 4]]])
 // getLength([1, [2, [3, [4, [5, 6]]]]])
 // getLength([1, [2], 1, [2], 1])
+
+// !________________________________________
+// ? write a function just to return element include nunmber
+// https://edabit.com/challenge/eCPim4XcssdZdvs32
+function numInStr(array) {
+    var check = array.filter(e => e.match(/\d/) ? e : null)
+    console.log(check);
+}
+
+numInStr(["1a", "a", "2b", "b"])
+numInStr(["abc", "abc10"])
+numInStr(["abc", "ab10c", "a10bc", "bcd"])
+numInStr(["this is a test", "test1"])
