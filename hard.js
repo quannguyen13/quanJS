891547a0e5fd05d85b77aefe2e9fa28478483c8e
quannguyen <waynenguyenus@gmail.com>
2022-05-11 20:40:54 -0700

    9

--- hard.js
+++ hard.js
@@ -41,13 +41,13 @@ function getStudentsWithNamesAndTopNotes(array){
     console.log(newArray)
 }
 
-// var result =
+
 getStudentsWithNamesAndTopNotes([
     { "name": "John", "notes": [3, 5, 4] },
     { "name": "Max", "notes": [1, 4, 6] },
     { "name": "Zygmund", "notes": [1, 2, 3] }
   ])
-// console.log(result);
+
 
 
 
