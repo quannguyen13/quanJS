318e1ddf95f0caee4cc6efc26308c802609456c0
quannguyen <waynenguyenus@gmail.com>
2022-06-13 03:31:41 -0700

    8

--- easyJS.js
+++ easyJS.js
@@ -490,3 +490,22 @@ function mean(number)   {
 // mean(42)
 // mean(12345)
 // mean(666)
+
+
+
+
+
+
+
+// Create a function that takes an array of numbers between 1 and 10 (excluding one number) and returns the missing number.
+
+// Examples
+// missingNum([1, 2, 3, 4, 6, 7, 8, 9, 10]) ➞ 5
+
+// missingNum([7, 2, 3, 6, 5, 9, 1, 4, 8]) ➞ 10
+
+// missingNum([10, 5, 1, 2, 4, 6, 8, 3, 9]) ➞ 7
+
+function missingNum(array) {
+    
+}
