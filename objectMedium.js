3eb24d3e8c1bb184097755633bf7ac826d4039c0
quannguyen <waynenguyenus@gmail.com>
2022-06-14 20:37:50 -0700

    4

--- objectMedium.js
+++ objectMedium.js
@@ -85,21 +85,21 @@ function afterNYears(obj, num) {
       console.log(obj);
 }
 
-afterNYears({
-    "Joel" : 32,
-    "Fred" : 44,
-    "Reginald" : 65,
-    "Susan" : 33,
-    "Julian" : 13
-  }, 1)
-afterNYears({
-    "Baby" : 2,
-    "Child" : 8,
-    "Teenager" : 15,
-    "Adult" : 25,
-    "Elderly" : 71
-  }, 19)
-afterNYears({
-    "Genie" : 1000,
-    "Joe" : 40
-  }, 5)
+// afterNYears({
+//     "Joel" : 32,
+//     "Fred" : 44,
+//     "Reginald" : 65,
+//     "Susan" : 33,
+//     "Julian" : 13
+//   }, 1)
+// afterNYears({
+//     "Baby" : 2,
+//     "Child" : 8,
+//     "Teenager" : 15,
+//     "Adult" : 25,
+//     "Elderly" : 71
+//   }, 19)
+// afterNYears({
+//     "Genie" : 1000,
+//     "Joe" : 40
+//   }, 5)
