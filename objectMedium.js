a908cef84077ce03066caf9dd05067a0cfb6da3b
quannguyen <waynenguyenus@gmail.com>
2022-06-15 20:27:47 -0700

    5

--- objectMedium.js
+++ objectMedium.js
@@ -142,6 +142,36 @@ function numbersSum(array) {
     console.log(sum);
 
 }
-numbersSum([1, 2, "13", "4", "645"])
-numbersSum([true, false, "123", "75"])
-numbersSum([1, 2, 3, 4, 5, true])
+// numbersSum([1, 2, "13", "4", "645"])
+// numbersSum([true, false, "123", "75"])
+// numbersSum([1, 2, 3, 4, 5, true])
+
+
+
+// An array is special if every even index contains an even number and every odd index contains an odd number. Create a function that returns true if an array is special, and false otherwise
+
+// ?https://edabit.com/challenge/wikfKaQm3zP7buRA4
+// ?https://stackoverflow.com/questions/7243355/how-do-i-extract-even-elements-of-an-array
+
+// Examples
+// isSpecialArray([2, 7, 4, 9, 6, 1, 6, 3]) ➞ true
+// Even indices: [2, 4, 6, 6]; Odd indices: [7, 9, 1, 3]
+
+// isSpecialArray([2, 7, 9, 1, 6, 1, 6, 3]) ➞ false
+// Index 2 has an odd number 9.
+
+// isSpecialArray([2, 7, 8, 8, 6, 1, 6, 3]) ➞ false
+// Index 3 has an even number 8.
+
+
+
+function isSpecialArray(array) {
+    for (let i = 0; i < array.length; i++) {
+        if (i % 2 == 0){
+            console.log(array[i]);
+        }
+    }
+}
+
+// isSpecialArray([2, 7, 4, 9, 6, 1, 6, 3])
+isSpecialArray([2, 7, 9, 1, 6, 1, 6, 3])
