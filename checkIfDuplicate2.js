e5874d80b232ec695aca11ada6a21236413f8c4c
quannguyen <waynenguyenus@gmail.com>
2022-07-23 14:31:41 -0700

    1

--- checkIfDuplicate2.js
+++ checkIfDuplicate2.js
@@ -18,6 +18,6 @@ var checkIfDuplicate = arr.some((ele, i) => arr.indexOf(ele) !== i)
 // console.log(checkIfDuplicate)
 // var check = arr.map((ele => arr.indexOf(ele)))
 // console.log(i);
-// console.log(check(arr));
+console.log(check(arr));
 
 
