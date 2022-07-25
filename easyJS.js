c103e0b4ec7ade663b6e7e5083d208d8a453a72a
quannguyen <waynenguyenus@gmail.com>
2022-07-24 21:22:47 -0700

    1

--- easyJS.js
+++ easyJS.js
@@ -772,4 +772,23 @@ function test() {
     var numberize = split.map(e => Number(e))
     console.log(numberize);
 }
-test(56812)
+// test(56812)
+
+function changeEnough(array, price) {
+    var quater = array[0] * .25
+    var dime = array[1] * .1
+    var nickel = array[2] * .05
+    var penny = array[3] * .01
+    var sum = quater + dime + nickel + penny
+    if (sum >= price) {
+        console.log(true);
+    } else {
+        console.log(false);
+        
+    }
+}
+changeEnough([2, 100, 0, 0], 14.11)
+changeEnough([0, 0, 20, 5], 0.75)
+changeEnough([30, 40, 20, 5], 12.55)
+changeEnough([10, 0, 0, 50], 3.85)
+changeEnough([1, 0, 5, 219], 19.99)
