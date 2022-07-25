8aa706ebed7a4e8228e9eaacb7be5796b7ef9e8b
quannguyen <waynenguyenus@gmail.com>
2022-07-24 21:27:44 -0700

    1

--- easyJS.js
+++ easyJS.js
@@ -787,8 +787,27 @@ function changeEnough(array, price) {
         
     }
 }
-changeEnough([2, 100, 0, 0], 14.11)
-changeEnough([0, 0, 20, 5], 0.75)
-changeEnough([30, 40, 20, 5], 12.55)
-changeEnough([10, 0, 0, 50], 3.85)
-changeEnough([1, 0, 5, 219], 19.99)
+// changeEnough([2, 100, 0, 0], 14.11)
+// changeEnough([0, 0, 20, 5], 0.75)
+// changeEnough([30, 40, 20, 5], 12.55)
+// changeEnough([10, 0, 0, 50], 3.85)
+// changeEnough([1, 0, 5, 219], 19.99)
+
+function changeEnough2(array, price) {
+    var array2 = [.25, .1, .05, .01]
+    var sum = 0
+    for (const j in array) {
+        sum += array[j]*array2[j];
+    }
+    if (sum >= price) {
+        console.log(true);
+    } else {
+        console.log(false);
+        
+    }
+}
+changeEnough2([2, 100, 0, 0], 14.11)
+changeEnough2([0, 0, 20, 5], 0.75)
+changeEnough2([30, 40, 20, 5], 12.55)
+changeEnough2([10, 0, 0, 50], 3.85)
+changeEnough2([1, 0, 5, 219], 19.99)
