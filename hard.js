f66ed9185c9945ee49059888f06895f78fc2a49f
quannguyen <waynenguyenus@gmail.com>
2022-07-26 22:37:11 -0700

    3

--- hard.js
+++ hard.js
@@ -261,7 +261,49 @@ function convert(str) {
         
     }
 }
-convert("35°C")
-convert("1353°C")
-convert("19°F")
-convert("19")
+// convert("35°C")
+// convert("1353°C")
+// convert("19°F")
+// convert("19")
+
+
+
+
+
+// Create a function that takes a string as an argument and returns a coded (h4ck3r 5p34k) version of the string.
+// In order to work properly, the function should replace all "a"s with 4, "e"s with 3, "i"s with 1, "o"s with 0, and "s"s with 5.
+// Examples
+// hackerSpeak("javascript is cool") ➞ "j4v45cr1pt 15 c00l"
+
+// hackerSpeak("programming is fun") ➞ "pr0gr4mm1ng 15 fun"
+
+// hackerSpeak("become a coder") ➞ "b3c0m3 4 c0d3r"
+
+
+
+function hackerSpeak(str) {
+    var array2 = [4, 3, 1, 0, 5]
+    var original = ["a", "e", "i", "o", "s"]
+    var manipulation = str.split("")
+    // console.log(manipulation);
+    for (let j = 0; j < manipulation.length; j++) {
+        for (let i = 0; i < original.length; i++) {
+            for (let k = 0; k < array2.length; k++) {
+                if (manipulation[j] == original[i]) {
+                    original[i] == array2[k]
+                    manipulation[j] == original[i]
+                    // console.log(manipulation[j]);
+                    return manipulation[j]
+                    // console.log(true);
+            }
+                
+            }
+
+        }
+
+}
+}
+console.log(
+
+    hackerSpeak("javascript is cool")
+);
