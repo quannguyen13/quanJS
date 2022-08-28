d89339d26bce602163f99889eae852a9e80b6a7e
quannguyen <waynenguyenus@gmail.com>
2022-08-28 10:19:24 -0700

    1

--- test2.js
+++ test2.js
@@ -1,5 +1,5 @@
 console.log("this is a test")
-var array = [1, 2, 3, 4, 5, 6, 7]
+// var array = [1, 2, 3, 4, 5, 6, 7]
 // var check = array.charAt(1)
 // console.log(check);
 
@@ -7,4 +7,15 @@ const sentence = 'The quick brown fox jumps over the lazy dog.';
 
 const index = 4;
 
-console.log(`The character at index ${index} is ${sentence.charAt(index)}`);
+// console.log(`The character at index ${index} is ${sentence.charAt(index)}`);
+
+
+var array = []
+for (let index = 1; index <= 10; index++) {
+    array.push(index)
+    
+}
+console.log(array);
+
+const sliced1 = array.slice(2, 4)
+console.log(sliced1);
