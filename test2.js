22b203a81771cc31152a29d9db4aecc84c485480
quannguyen <waynenguyenus@gmail.com>
2022-09-10 10:35:15 -0700

    1

--- test2.js
+++ test2.js
@@ -1,4 +1,4 @@
-console.log("this is a test")
+// console.log("this is a test")
 // var array = [1, 2, 3, 4, 5, 6, 7]
 // var check = array.charAt(1)
 // console.log(check);
@@ -15,13 +15,13 @@ for (let index = 1; index <= 10; index++) {
     array.push(index)
     
 }
-console.log(array);
+// console.log(array);
 
 // const sliced1 = array.slice(2, 4)
 
 array.forEach((e, index, newArry) => newArry[index] = e * 2 )
 
-console.log(array);
+// console.log(array);
 
 
 var students = ["Dale", "Juan", "Quan"]
@@ -31,3 +31,22 @@ students.forEach((item, index, arr) => arr[index] = "fuck " + item )
 var map = students.map(e => "fuck " + e)
 // console.log(map);
 
+function count() {
+    var newArray1 = []
+    var newArray2 = []
+    var split = sentence.split("")
+    console.log(split);
+    for (let i = 0; i < split.length; i++) {
+        if (split[i] == "o") {
+            newArray1.push(split[i])
+        }  
+    }
+    for (let j = 0; j < split.length; j++){
+        if (split[j] == "e") {
+            newArray2.push(split[j])
+        }
+    }
+
+    console.log([newArray1.length, newArray2.length]);
+}
+count(sentence)
